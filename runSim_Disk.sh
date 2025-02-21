e89001220363afd551c636e936721e2ba52cc02e
Samll321 <ons5@case.edu>
2025-02-21 12:12:01 -0500

    For Sim_Trilayer, Added new simulation type to simulate only one disk system. For runSim_Disk, adjusted the range avf2odt is averaging over, this should fix a lot of issues.

---
+++ runSim_Disk.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=100GB 
+#SBATCH --time=23:59:00
+
+cd ~/Desktop/oommf
+
+tclsh oommf.tcl boxsi "Projects/Trilayer/Simulation/Sim_Trilayer.mif" -parameters "SimType 1 pt $1 qt $2" -restart 0 -threads 20
+
+cd Projects/Trilayer/Simulation
+
+mv -i SimTrilayerTopM0*.omf m0filetop.omf
+
+cd ~/Desktop/oommf
+
+tclsh oommf.tcl boxsi "Projects/Trilayer/Simulation/Sim_Trilayer.mif" -parameters "SimType 4" -restart 0 -threads 20
+
+tclsh oommf.tcl avf2odt -average "point" -headers "none" -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_RungeKuttaEvolve-evolver-Total_energy_density-*-*.oef"
+
+tclsh oommf.tcl avf2odt -average "point" -headers "none" -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_TimeDriver-Magnetization-*-*.omf" 
+
+cd Projects/Trilayer/Simulation
+
+mv -i SimTrilayer-*.odt ../
+
+cd ../
+
+mv -i SimTrilayer-Oxs_RungeKuttaEvolve-*.odt Energy\ Data\ Total
+
+mv -i SimTrilayer-Oxs_TimeDriver-*.odt Magnetization\ Data\ Total
+
+cd ~
+
+done
--- runSim_Trilayer.sh
+++ runSim_Trilayer.sh
@@ -37,9 +37,9 @@ mkdir Projects/Trilayer/Energy\ Data\ Total
 
 mkdir Projects/Trilayer/Magnetization\ Data\ Total
 
-tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 0.0 5.0e-8 5.0e-8 1.0e-9 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_RungeKuttaEvolve-evolver-Total_energy_density-*-*.oef"
+tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 3.0e-9 5.0e-8 5.0e-8 0.0 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_RungeKuttaEvolve-evolver-Total_energy_density-*-*.oef"
 
-tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 0.0 5.0e-8 5.0e-8 1.0e-9 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_TimeDriver-Magnetization-*-*.omf" 
+tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 3.0e-9 5.0e-8 5.0e-8 0.0 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_TimeDriver-Magnetization-*-*.omf" 
 
 cd Projects/Trilayer/Simulation
 
@@ -53,9 +53,9 @@ mv -i SimTrilayer-Oxs_TimeDriver-*.odt Magnetization\ Data\ Top
 
 cd ~/Desktop/oommf
 
-tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 0.0 5.0e-8 5.0e-8 -1.0e-9 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_RungeKuttaEvolve-evolver-Total_energy_density-*-*.oef"
+tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 -1.0e-9 5.0e-8 5.0e-8 -3.0e-9 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_RungeKuttaEvolve-evolver-Total_energy_density-*-*.oef"
 
-tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 0.0 5.0e-8 5.0e-8 -1.0e-9 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_TimeDriver-Magnetization-*-*.omf" 
+tclsh oommf.tcl avf2odt -average "line" -axis "z" -headers "none" -region -5.0e-8 -5.0e-8 -1.0e-9 5.0e-8 5.0e-8 -3.0e-9 -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_TimeDriver-Magnetization-*-*.omf" 
 
 cd Projects/Trilayer/Simulation
 
