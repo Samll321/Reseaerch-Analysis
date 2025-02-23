ba6f16a881f9ef3be218e7be1b56be0c148c1ecc
Samll321 <ons5@case.edu>
2025-02-23 15:58:24 -0500

    Changed averageing z limits in avf2odt to zmin to z max since that is what oommf takes

--- runSim_Disk.sh
+++ runSim_Disk.sh
@@ -15,6 +15,10 @@ mv -i SimTrilayerTopM0*.omf m0filetop.omf
 
 cd ~/Desktop/oommf
 
+mkdir Projects/Trilayer/Energy\ Data\ Total
+
+mkdir Projects/Trilayer/Magnetization\ Data\ Total
+
 tclsh oommf.tcl boxsi "Projects/Trilayer/Simulation/Sim_Trilayer.mif" -parameters "SimType 4" -restart 0 -threads 20
 
 tclsh oommf.tcl avf2odt -average "point" -headers "none" -ipat "Projects/Trilayer/Simulation/SimTrilayer-Oxs_RungeKuttaEvolve-evolver-Total_energy_density-*-*.oef"
